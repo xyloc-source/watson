82dbad7787485c1648c88c889018492353589a80
pokai <help@aiinpocket.com>
2025-12-19 00:13:33 +0800

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,81 @@
+AOS.init({ startEvent: "load" });
+window.addEventListener("load", AOS.refresh);
+
+document.querySelectorAll(".header-lang-active").forEach(function (element) {
+  element.addEventListener("click", function () {
+    const parent = this.closest(".header__lang");
+    if (parent) {
+      parent.classList.toggle("active");
+    }
+  });
+});
+
+function handleScrollResize() {
+  if (window.scrollY > 0) {
+    document.querySelectorAll(".header").forEach(function (el) {
+      el.classList.add("scrolldown");
+    });
+  } else {
+    document.querySelectorAll(".header").forEach(function (el) {
+      el.classList.remove("scrolldown");
+    });
+  }
+}
+
+window.addEventListener("scroll", handleScrollResize);
+window.addEventListener("resize", handleScrollResize);
+
+var swiper1 = new Swiper("#swiper-1", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper2 = new Swiper("#swiper-2", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper2 = new Swiper("#swiper-3", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper2 = new Swiper("#swiper-4", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper2 = new Swiper("#swiper-5", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
